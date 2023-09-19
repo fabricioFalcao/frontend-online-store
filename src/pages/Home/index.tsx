bbc4f774a9a96f658d72f5f914f463be350f6164
fabur13 <fabur13@gmail.com>
2023-09-19 17:59:24 -0300

    feat: Requisito 03

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,15 @@
+import { Link } from 'react-router-dom';
+
 function Home() {
   return (
-    <h1 data-testid="home-initial-message">
-      Digite algum termo de pesquisa ou escolha uma categoria.
-    </h1>
+    <div>
+      <h1 data-testid="home-initial-message">
+        Digite algum termo de pesquisa ou escolha uma categoria.
+      </h1>
+      <Link to="/cart" data-testid="shopping-cart-button">
+        <h1>Para o Carrinho</h1>
+      </Link>
+    </div>
   );
 }
 
