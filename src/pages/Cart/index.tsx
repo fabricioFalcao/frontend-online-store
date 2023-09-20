02d368dc168c791fb157e6e12e878a0a2e7375f9
fabricioFalcao <fabriciohfd@gmail.com>
2023-09-20 16:23:05 -0300

    Requisito 8 finalizado

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -1,7 +1,47 @@
+import { Link } from 'react-router-dom';
+
 function ShoppingCart() {
+  const cart = JSON.parse(localStorage.getItem('cartList') || '[]');
+
   return (
     <div>
-      <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
+      {cart.length === 0 ? (
+        <h2 data-testid="shopping-cart-empty-message">Seu carrinho está vazio</h2>
+      ) : (
+        cart.map(({ id, title, thumbnail, price, counter }: any) => {
+          return (
+            <div
+              key={ id }
+            >
+              <Link
+                to={ `/product/${id}` }
+                data-testid="product-detail-link"
+              >
+                <div
+                  data-testid="product"
+                >
+                  <h2
+                    data-testid="shopping-cart-product-name"
+                  >
+                    { title }
+
+                  </h2>
+                  <img src={ thumbnail } alt={ title } />
+                  <p>{ price }</p>
+                </div>
+              </Link>
+              <p
+                data-testid="shopping-cart-product-quantity"
+              >
+
+                {counter}
+
+              </p>
+            </div>
+          );
+        })
+      )}
+
     </div>
   );
 }
